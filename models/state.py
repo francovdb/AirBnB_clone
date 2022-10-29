b3396b8bfd2fe576ef6299c70786985ad8704932
Hassan-Hajir <hashajir@.com>
2022-10-28 21:43:47 -0700

    models a City and inherits from Base Model

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"" Contains the State model"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Implements the State model for any state object"""
+    name = ""
